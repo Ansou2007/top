c0a31ace2d6e6bcd483604c132f32eb721f92aaf
ansou2007 <ansoumanemichel.tamba@uvs.edu.sn>
2021-12-06 13:44:06 +0000

    Mise à jour

--- utilisateur/core/connection.php
+++ utilisateur/core/connection.php
@@ -12,29 +12,7 @@ $modpass="";
 	}
 
 
-/*
-	class Model{
-		private $serveur = "localhost";
-		private $database = "pastef";
-		private $utilisateur = "root";
-		private $mdp ="";
-		private $con;
 
-		public function __construct(){
-
-		try{
-
-			$this->con=new pdo("mysql:host=$this->serveur;dname=$this->database",$this->utilisateur,$this->mdp);
-
-		}catch(PDOException $e){
-			Die('Erreur:'.$e->getMessage());
-		}
-			
-		} 
-
-    
-	}
-	*/
 	
 
 
